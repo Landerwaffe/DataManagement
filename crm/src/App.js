9657b6e25087a8f2c1c024b4e045299c274c242f
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2024-06-10 15:06:43 +0700

    Text Field Import

--- crm/src/App.js
+++ crm/src/App.js
@@ -1,14 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ButtonUsage from "./Button";
+import TextField from "@mui/material/TextField";
 
 function App() {
   return (
-    // <div className="App">
-    <header className="App-header">
-      <ButtonUsage />
-    </header>
-    // </div>
+    <div className="App">
+      <header className="App-header">
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="standard-basic" label="Standard" variant="standard" />
+        <ButtonUsage />
+      </header>
+    </div>
   );
 }
 
