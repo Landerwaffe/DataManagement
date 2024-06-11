ddbf4b197bf4d6a6cd94f0363bf26c632c47d269
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2024-06-11 15:50:09 +0700

    Style changes

--- crm/src/App.js
+++ crm/src/App.js
@@ -9,10 +9,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h3>Login</h3>
         <FormControl>
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-          <TextField id="filled-basic" label="Filled" variant="filled" />
-          <TextField id="standard-basic" label="Standard" variant="standard" />
+          <TextField id="outlined-basic" label="Email" variant="outlined" />
+          <TextField id="outlined-basi" label="Password" variant="outlined" />
+          {/* <TextField id="filled-basic" label="Filled" variant="filled" /> */}
+          {/* <TextField id="standard-basic" label="Standard" variant="standard" /> */}
           <ButtonUsage />
         </FormControl>
       </header>
