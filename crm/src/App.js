ab99addddf9b5e5c8e45dbfa9f9d8da3a97ea69c
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2024-06-11 15:31:36 +0700

    More Material UI Import

--- crm/src/App.js
+++ crm/src/App.js
@@ -2,15 +2,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import ButtonUsage from "./Button";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { FormControl, FormLabel } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="filled-basic" label="Filled" variant="filled" />
-        <TextField id="standard-basic" label="Standard" variant="standard" />
-        <ButtonUsage />
+        <FormControl>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          <TextField id="filled-basic" label="Filled" variant="filled" />
+          <TextField id="standard-basic" label="Standard" variant="standard" />
+          <ButtonUsage />
+        </FormControl>
       </header>
     </div>
   );
